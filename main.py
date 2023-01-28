7ed78ec0ac2c78fe7110822e0521048d1d72c81a
Vyacheslav <danubise@gmail.com>
2023-01-28 16:43:11 +0500

    Added print out title of the video file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def process(youtube_url: str = None):
     print(f"Will process '{youtube_url}'")
     yt = YouTube(youtube_url)
     print("Downloading ...")
+    print(f"Video title '{yt.title}'")
     yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(
         output_path="download")
     print("Done.")
